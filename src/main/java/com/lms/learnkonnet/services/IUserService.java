0b16c169c071dc4b68c85bc3a37640e19b3dc183
NguyenHungKhang <104726874+NguyenHungKhang@users.noreply.github.com>
2024-04-03 21:28:04 +0700

    add structure for service

--- src/main/java/com/lms/learnkonnet/services/IUserService.java
+++ src/main/java/com/lms/learnkonnet/services/IUserService.java
@@ -2,8 +2,19 @@ package com.lms.learnkonnet.services;
 
 
 import com.lms.learnkonnet.dtos.requests.user.CreateUserRequestDto;
+import com.lms.learnkonnet.dtos.requests.user.UpdateUserRequestDto;
+import com.lms.learnkonnet.dtos.responses.common.PageResponse;
 import com.lms.learnkonnet.dtos.responses.user.UserDetailResponseDto;
+import jakarta.persistence.criteria.Order;
+
+import java.util.List;
 
 public interface IUserService {
-    public UserDetailResponseDto add(CreateUserRequestDto user);
+    PageResponse<UserDetailResponseDto> getList(List<Order> orders, int page, int size);
+    UserDetailResponseDto getById(Long id);
+    UserDetailResponseDto getByEmail(String email);
+    UserDetailResponseDto add(CreateUserRequestDto user);
+    UserDetailResponseDto update(Long id, UpdateUserRequestDto user);
+    Boolean softDelete(Long id);
+    Boolean delete(Long id);
 }
