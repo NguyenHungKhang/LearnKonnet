f8ded428775670e92b2a52fd9bf0a96b720ea7db
NguyenHungKhang <104726874+NguyenHungKhang@users.noreply.github.com>
2024-04-07 16:57:44 +0700

    add config

---
+++ src/main/java/com/lms/learnkonnet/securities/MyUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.lms.learnkonnet.securities;
+
+import com.lms.learnkonnet.models.User;
+import com.lms.learnkonnet.repositories.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private IUserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + username));
+		return user;
+	}
+
+}
