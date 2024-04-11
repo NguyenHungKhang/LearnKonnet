cf790f28b6104610e660da0753ba1038819fbcdb
NguyenHungKhang <104726874+NguyenHungKhang@users.noreply.github.com>
2024-04-12 00:00:27 +0700

    complete topic controller

--- src/main/java/com/lms/learnkonnet/controllers/TopicController.java
+++ src/main/java/com/lms/learnkonnet/controllers/TopicController.java
@@ -1,25 +1,25 @@
 package com.lms.learnkonnet.controllers;
 
-import com.lms.learnkonnet.dtos.requests.course.CourseRequestDto;
 import com.lms.learnkonnet.dtos.requests.member.MemberRequestDto;
+import com.lms.learnkonnet.dtos.requests.topic.TopicRequestDto;
 import com.lms.learnkonnet.dtos.responses.common.PageResponse;
-import com.lms.learnkonnet.dtos.responses.course.CourseDetailResponseDto;
-import com.lms.learnkonnet.dtos.responses.course.CourseSumaryResponseDto;
 import com.lms.learnkonnet.dtos.responses.member.MemberBasicInfoResponseDto;
 import com.lms.learnkonnet.dtos.responses.member.MemberDetailResponseDto;
+import com.lms.learnkonnet.dtos.responses.topic.TopicBasicInfoResponseDto;
+import com.lms.learnkonnet.dtos.responses.topic.TopicDetailResponseDto;
 import com.lms.learnkonnet.exceptions.ApiResponse;
-import com.lms.learnkonnet.exceptions.ResourceNotFoundException;
-import com.lms.learnkonnet.models.Course;
-import com.lms.learnkonnet.models.Member;
 import com.lms.learnkonnet.models.enums.MemberStatus;
 import com.lms.learnkonnet.models.enums.MemberType;
 import com.lms.learnkonnet.repositories.ICourseRepository;
 import com.lms.learnkonnet.repositories.IMemberRepository;
+import com.lms.learnkonnet.repositories.ITopicRepository;
 import com.lms.learnkonnet.services.ICourseService;
 import com.lms.learnkonnet.services.IMemberService;
+import com.lms.learnkonnet.services.ITopicService;
 import com.lms.learnkonnet.services.IUserService;
 import com.lms.learnkonnet.services.impls.CourseService;
 import com.lms.learnkonnet.services.impls.MemberService;
+import com.lms.learnkonnet.services.impls.TopicService;
 import com.lms.learnkonnet.services.impls.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -28,11 +28,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/member")
-public class MemberController {
+@RequestMapping("/api/v1/topic")
+public class TopicController {
     @Autowired
     private ICourseService courseService = new CourseService();
     @Autowired
@@ -40,70 +39,66 @@ public class MemberController {
     @Autowired
     private IMemberService memberService = new MemberService();
     @Autowired
-    private ICourseRepository courseRepository;
-    @Autowired
-    private IMemberRepository memberRepository;
-    // create
+    private ITopicService topicService = new TopicService();
+
     @PostMapping("/")
-    public ResponseEntity<?> add(@RequestBody MemberRequestDto member, Principal principal) {
+    public ResponseEntity<?> add(@RequestBody TopicRequestDto topic, Principal principal) {
         Long currentUserId = userService.getIdByEmail(principal.getName());
-        MemberDetailResponseDto newMember = memberService.add(member, currentUserId);
-        return new ResponseEntity<MemberDetailResponseDto>(newMember, HttpStatus.CREATED);
+        TopicBasicInfoResponseDto newTopic = topicService.add(topic, currentUserId);
+        return new ResponseEntity<TopicBasicInfoResponseDto>(newTopic, HttpStatus.CREATED);
     }
 
     // update
     @PostMapping("/{id}")
-    public ResponseEntity<?> update(@RequestBody MemberRequestDto member, @PathVariable Long id, Principal principal) {
+    public ResponseEntity<?> update(@RequestBody TopicRequestDto topic, @PathVariable Long id, Principal principal) {
         Long currentUserId = userService.getIdByEmail(principal.getName());
-        MemberDetailResponseDto newMember = memberService.update(id, member, currentUserId);
-        return new ResponseEntity<MemberDetailResponseDto>(newMember, HttpStatus.OK);
+        TopicBasicInfoResponseDto updatedTopic = topicService.update(id, topic, currentUserId);
+        return new ResponseEntity<TopicBasicInfoResponseDto>(updatedTopic, HttpStatus.OK);
     }
 
     // soft delete
     @PutMapping("/soft-delete/{id}")
     public ResponseEntity<?> softDelete(@PathVariable Long id, Principal principal) {
         Long currentUserId = userService.getIdByEmail(principal.getName());
-        Boolean isSoftDeletedMember = memberService.softDelete(id, currentUserId);
-        return new ResponseEntity<ApiResponse>(new ApiResponse("Member soft deleted stauts: " + isSoftDeletedMember, true), HttpStatus.OK);
+        Boolean isSoftDeletedTopic = topicService.softDelete(id, currentUserId);
+        return new ResponseEntity<ApiResponse>(new ApiResponse("Member soft deleted stauts: " + isSoftDeletedTopic, true), HttpStatus.OK);
     }
 
     // delete
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id, Principal principal) {
         Long currentUserId = userService.getIdByEmail(principal.getName());
-        Boolean isSoftDeletedMember = memberService.delete(id, currentUserId);
-        return new ResponseEntity<ApiResponse>(new ApiResponse("Member deleted status: " + isSoftDeletedMember, true), HttpStatus.OK);
+        Boolean isDeletedTopic = topicService.delete(id, currentUserId);
+        return new ResponseEntity<ApiResponse>(new ApiResponse("Member deleted status: " + isDeletedTopic, true), HttpStatus.OK);
     }
 
-    @GetMapping("/list/course/{courseId}")
+    @GetMapping("/list-pageable/course/{courseId}")
     public ResponseEntity<?> getAllPageableListByMemberTypeAndMemberStatus(
             @RequestParam(required = false, defaultValue = "") String keyword,
             @RequestParam(required = false, defaultValue = "createdAt") String sortField,
             @RequestParam(required = false, defaultValue = "asc") String sortDir,
             @RequestParam(defaultValue = "0") int pageNum,
             @RequestParam(defaultValue = "10") int pageSize,
-            @RequestParam(defaultValue = "ACTIVED") MemberStatus status,
-            @RequestParam(defaultValue = "STUDENT") MemberType type,
             @PathVariable Long courseId,
             Principal principal) {
         Long currentUserId = userService.getIdByEmail(principal.getName());
-        PageResponse<MemberBasicInfoResponseDto> members = memberService.getStudentInfoPageableListByMemberAndType(
-                keyword, sortField, sortDir, pageNum, pageSize, currentUserId, courseId, status, type);
-        return new ResponseEntity<PageResponse<MemberBasicInfoResponseDto>>(members, HttpStatus.OK);
+        PageResponse<TopicDetailResponseDto> topics = topicService.getPageableList(
+                keyword, sortField, sortDir, pageNum, pageSize, currentUserId, courseId);
+        return new ResponseEntity<PageResponse<TopicDetailResponseDto>>(topics, HttpStatus.OK);
     }
 
-    @GetMapping("/list/course/{courseId}/student")
+    @GetMapping("/list/course/{courseId}")
     public ResponseEntity<?> getAllStudent(@PathVariable Long courseId, Principal principal) {
         Long currentUserId = userService.getIdByEmail(principal.getName());
-        List<MemberBasicInfoResponseDto> member = memberService.getAllStudentInfo(courseId, currentUserId);
-        return new ResponseEntity<List<MemberBasicInfoResponseDto>>(member, HttpStatus.OK);
+        List<TopicDetailResponseDto> topics = topicService.getAll(courseId, currentUserId);
+        return new ResponseEntity<List<TopicDetailResponseDto>>(topics, HttpStatus.OK);
     }
 
     // get by id
     @GetMapping("/{id}")
     public ResponseEntity<?> getOne(@PathVariable Long id, Principal principal) {
         Long currentUserId = userService.getIdByEmail(principal.getName());
-        MemberDetailResponseDto member = memberService.getDetailById(id, currentUserId);
-        return new ResponseEntity<MemberDetailResponseDto>(member, HttpStatus.OK);
+        TopicDetailResponseDto topic = topicService.getById(id, currentUserId);
+        return new ResponseEntity<TopicDetailResponseDto>(topic, HttpStatus.OK);
     }
 }
--- src/main/java/com/lms/learnkonnet/repositories/ITopicRepository.java
+++ src/main/java/com/lms/learnkonnet/repositories/ITopicRepository.java
@@ -2,15 +2,20 @@ package com.lms.learnkonnet.repositories;
 
 import com.lms.learnkonnet.models.Course;
 import com.lms.learnkonnet.models.Topic;
+import com.lms.learnkonnet.models.enums.Status;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ITopicRepository extends JpaRepository<Topic, Long> {
     Page<Topic> findByCourse_IdAndNameContaining(Long courseId, String keyword, Pageable pageable);
     List<Topic> findAllByCourse_Id(Long courseId);
+    Page<Topic> findByCourse_IdAndNameContainingAndStatusAndIsDeletedFalse(Long courseId, String keyword, Status status, Pageable pageable);
+    List<Topic> findAllByCourse_IdAndStatusAndIsDeletedFalse(Long courseId, Status status);
+    Optional<Topic> findByIdAndStatusAndIsDeletedFalse(Long id, Status status);
 }
